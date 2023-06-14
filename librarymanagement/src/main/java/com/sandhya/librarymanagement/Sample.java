30a1cbd9735ed131c56d01481fdd593a82ecedc8
Sandhya0609 <sandhya0609a@gmail.com>
2023-06-14 11:40:51 -0400

    Adding delete/update methods to Book api

--- librarymanagement/src/main/java/com/sandhya/librarymanagement/Sample.java
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/Sample.java
@@ -1,13 +1,18 @@
 package com.sandhya.librarymanagement;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Sample {
 
+//@Value("${spring.info}")
+    private String message=" Welcome Java";
+
 @GetMapping("/hello")
     public String getInfo(){
-        return "Welcome to Spring ";
+
+    return this.message;
     }
 }
