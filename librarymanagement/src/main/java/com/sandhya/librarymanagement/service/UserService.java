c855052aaaf3581e5c1ac32043a4f2588817125e
Sandhya0609 <sandhya0609a@gmail.com>
2023-06-15 12:51:36 -0400

    Initial commit to User Email API

--- librarymanagement/src/main/java/com/sandhya/librarymanagement/service/UserService.java
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/service/UserService.java
@@ -1,11 +1,31 @@
 package com.sandhya.librarymanagement.service;
 
+import com.sandhya.librarymanagement.model.User;
 import com.sandhya.librarymanagement.repository.UserRepository;
+import com.sandhya.librarymanagement.util.SendRegistrationSuccessful;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String registerUser(User user){
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        this.userRepository.save(user);
+        SendRegistrationSuccessful sendRegistrationSuccessful=new SendRegistrationSuccessful();
+        String info="You have successfully registered in to library management website";
+        sendRegistrationSuccessful.sendAnEmail(user.getEmail(),info);
+
+        return info;
+
+    }
 
 }
