ad5e577641e1fa9ff560855cac4caa683d74729a
Sandhya0609 <sandhya0609a@gmail.com>
2023-06-16 13:02:54 -0400

    Second commit to User Email API

--- librarymanagement/src/main/java/com/sandhya/librarymanagement/service/UserService.java
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/service/UserService.java
@@ -2,28 +2,35 @@ package com.sandhya.librarymanagement.service;
 
 import com.sandhya.librarymanagement.model.User;
 import com.sandhya.librarymanagement.repository.UserRepository;
-import com.sandhya.librarymanagement.util.SendRegistrationSuccessful;
+import com.sandhya.librarymanagement.util.SendRegistrationSuccessMail;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
+    private SendRegistrationSuccessMail sendRegistrationSuccessMail;
+
     private PasswordEncoder passwordEncoder;
 
-    public String registerUser(User user){
+    public String registerUser(User user) {
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         this.userRepository.save(user);
-        SendRegistrationSuccessful sendRegistrationSuccessful=new SendRegistrationSuccessful();
-        String info="You have successfully registered in to library management website";
-        sendRegistrationSuccessful.sendAnEmail(user.getEmail(),info);
 
+        String info="You have successfully registered in to library management website";
+        try {
+            sendRegistrationSuccessMail.sendAnEmail(user.getEmail(),info);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
         return info;
 
     }
