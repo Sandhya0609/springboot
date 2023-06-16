095f62314855594016be44d35d33f3920bc3c418
Sandhya0609 <sandhya0609a@gmail.com>
2023-06-16 13:03:28 -0400

    Second commit to User Email API

---
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.sandhya.librarymanagement.controller;
+
+
+import com.sandhya.librarymanagement.model.User;
+import com.sandhya.librarymanagement.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String registerAndSendAnEmail(@RequestBody User user){
+        return  userService.registerUser(user);
+    }
+}
---
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/util/SendRegistrationSuccessMail.java
@@ -0,0 +1,59 @@
+package com.sandhya.librarymanagement.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SendRegistrationSuccessMail {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
+
+    public SendRegistrationSuccessMail() {
+    }
+
+    public String sendAnEmail(String email, String Message) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            Map<String, Object> mydata = new HashMap<String, Object>();
+            mydata.put("username", email);
+
+            helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED, StandardCharsets.UTF_8.name());
+
+            Context context = new Context();
+            context.setVariables(mydata);
+
+            helper.setFrom("sandhya0609a@gmail.com");
+            helper.setTo(email);
+            helper.setSubject("Registration Successful");
+
+
+            String template = springTemplateEngine.process("registration", context);
+            helper.setText(template, true);
+            javaMailSender.send(mimeMessage);
+
+            return "sent message";
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw  e;
+        }
+    }
+
+}
+
+
