340452af966392a56640062bed5618c0ca96b5b0
Sandhya0609 <sandhya0609a@gmail.com>
2023-06-22 16:46:03 -0400

    First Commit JWT Token

--- librarymanagement/src/main/java/com/sandhya/librarymanagement/config/JwtAuthenticationFilter.java
+++ librarymanagement/src/main/java/com/sandhya/librarymanagement/config/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.io.IOException;
 
@@ -26,5 +27,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+
+        if(request.getServletPath().contains("")){
+            filterChain.doFilter(request,response);
+        }
+
+        final String authHeader=request.getHeader("Authorization");
+
+        final  String jwt;
+        final String userEmail;
+
+        if(authHeader==null || !authHeader.startsWith("Bearer")){
+            return;
+        }
+        jwt=authHeader.substring(7);
+        userEmail=jwtService.extractUsername(jwt);
     }
 }
